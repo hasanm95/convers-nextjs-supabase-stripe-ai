64d322ec355d027f3eaa8718eba8db5dcba39657
hasanm95 <hsncool10@gmail.com>
2025-12-07 16:08:13 +0800

    added navbar

---
+++ components/nav-bar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import NavItems from "@/components/nav-items";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <button className="btn-signin">Sign In</button>
+        <div>user</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
